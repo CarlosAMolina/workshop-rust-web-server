73ccfcecc784872e74c07bdd8d2874445c438965
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-04-27 20:16:33 +0200

    Fix rust ownership example

--- code/ownership/src/main.rs
+++ code/ownership/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let tribe_name = "Back";
+    let tribe_name = String::from("Backend");
     println!("Hello {} tribe!", tribe_name);
     greet_tribe(tribe_name);
-    println!("Hello {} tribe!", tribe_name);
+    // println!("Hello {} tribe!", tribe_name); // ERROR
 }
 
-fn greet_tribe(name: &str) {
+fn greet_tribe(name: String) {
     println!("Hello {} tribe!", name);
 }
 
