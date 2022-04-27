54905a2b9c4634d84b516636e8186f45d1853bda
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-04-27 20:52:13 +0200

    Improve rust code with ownership and borrwing

--- code/ownership/src/main.rs
+++ code/ownership/src/main.rs
@@ -1,11 +1,16 @@
 fn main() {
     let tribe_name = String::from("Backend");
-    println!("Hello {} tribe!", tribe_name);
-    greet_tribe(tribe_name);
-    // println!("Hello {} tribe!", tribe_name); // ERROR
+    println!("Hi {} Tribe!", tribe_name);
+    greet_borrowing(&tribe_name);
+    println!("Hi {} Tribe!", tribe_name);
+    greet_takes_ownership(tribe_name);
+    //println!("Hello {} Tribe!", tribe_name); // ERROR
 }
 
-fn greet_tribe(name: String) {
-    println!("Hello {} tribe!", name);
+fn greet_borrowing(name: &String) {
+    println!("Hi {} Tribe!", name);
 }
 
+fn greet_takes_ownership(name: String) {
+    println!("Hi {} Tribe!", name);
+}
